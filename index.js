90d03e17b752423df56fe9ba6c3bbcacd6993d6b
JasonWilson9 <84356946+jrwilson89@users.noreply.github.com>
2021-12-02 21:21:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const input = require("readline-sync");
+
+// function initialPrompt () {
+//   let response=input.question("Want to find the area of a triangle?  Enter the base: ")
+//     response=
+// }
+
+function triArea(base, height) {
+  let area=((base*height)/2)
+  return area
+	
+}
